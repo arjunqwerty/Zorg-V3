3abf5d3d7b42899ccf4da30a9497ee57c5111e00
Zorg66 <zorg123546@gmail.com>
2020-09-16 17:04:47 +0530

    merged register customer table and custprof table

--- zorg.py
+++ zorg.py
@@ -74,7 +74,7 @@ def registermnmg():
             flash("Username already exists", 'danger')
     return render_template('remnmg.html')
 
-class RegisterCustomer(db.Model):
+class custdetails(db.Model):
     __tablename__ = 'custdetails'
     namecust = db.Column(db.String(200))
     username = db.Column(db.String(200), primary_key=True)
@@ -82,26 +82,33 @@ class RegisterCustomer(db.Model):
     pincode = db.Column(db.Integer())
     address = db.Column(db.Text())
     gmail_id = db.column(db.String(200))
+    aadhar = db.Column(db.Integer, unique=True)
+    age = db.Column(db.Integer)
+    gender = db.Column(db.String(1))
+    prevmedrcrds = db.Column(db.Text())
 
-    def __init__(self, namecust, username, password, pincode, address, gmail_id):
+    def __init__(self, namecust, username, password, pincode, address, gmail_id, aadhar, age, gender, prevmedrcrds):
         self.namecust = namecust
         self.username = username
         self.password = password
         self.pincode = pincode
         self.address = address
         self.gmail_id = gmail_id
-@app.route('/registercustomer', methods=['GET','POST'])
-def registercustomer():
+        self.aadhar = aadhar
+        self.age = age
+        self.gender = gender
+        self.prevmedrcrds = prevmedrcrds
+
+@app.route('/custdetails', methods=['GET','POST'])
+def custdetails():
     if request.method == 'POST':
         namecust = request.form['namecust'] 
         username = request.form['username']
         password = request.form['password']
-        pincode = request.form['pincode'] 
-        address = request.form['address']
         gmail_id = request.form['gmail_id']
 
-        if db.session.query(RegisterCustomer).filter(RegisterCustomer.username == username).count() == 0:
-            data = RegisterCustomer(namecust, username, password, pincode, address)
+        if db.session.query(custdetails).filter(custdetails.username == username).count() == 0:
+            data = custdetails(namecust, username, password, gmail_id)
             db.session.add(data)
             db.session.commit()
             flash('you are now registered', 'success')
@@ -135,7 +142,7 @@ def logincustomer():
     if request.method == 'POST':
         usercust = request.form['username']
         password_candidate = request.form['password']
-        user = db.session.query(RegisterCustomer).filter(RegisterCustomer.username == usercust).first()
+        user = db.session.query(custdetails).filter(custdetails.username == usercust).first()
         db.session.commit()
         if password_candidate == user.password:
             session['logged_in'] = True
@@ -167,29 +174,11 @@ def logout():
     flash('You are now logged out','success')
     return redirect(url_for('home'))
 
-class custprof(db.Model):
-    __tablename__ = 'custprof'
-    username = db.Column(db.String(200))
-    name = db.Column(db.String(200))
-    aadhar = db.Column(db.Integer, primary_key=True)
-    age = db.Column(db.Integer)
-    gender = db.Column(db.String(1))
-    prevmedrcrds = db.Column(db.Text())
-    address = db.Column(db.Text())
-    pincode = db.Column(db.Integer)
-    
-    def __init__(self, name, aadhar, age, gender, prevmedrcrds, pincode):
-        self.name = name
-        self.aadhar = aadhar
-        self.age = age
-        self.gender = gender
-        self.prevmedrcrds = prevmedrcrds
-        self.pincode = pincode
 @app.route('/dashboard', methods=['GET','POST'])
 @is_logged_in
 def dashboard():
     username = session['username']
-    profile = db.session.query(custprof).filter(custprof.username == username).first()#111
+    profile = db.session.query(custdetails).filter(custdetails.username == username).first()#111
     db.session.commit()
     if profile is not None:
         return render_template('dashboard.html')
@@ -208,8 +197,8 @@ def add_profile():
         prevmedrcrds = request.form['prevmedrcrds']
         address = request.form['address']
         pincode = request.form['pincode']
-        if db.session.query(custprof).filter(custprof.username == username).count() == 0:
-            data = custprof(name, aadhar, age, gender,prevmedrcrds, address, pincode)
+        if db.session.query(custdetails).filter(custdetails.username == username).count() == 0:
+            data = custdetails(name, aadhar, age, gender,prevmedrcrds, address, pincode)
             db.session.add(data)
             db.session.commit()
             flash('Profile Created', 'success')
@@ -222,7 +211,7 @@ def add_profile():
 @is_logged_in
 def editprofile():
     username = session['username']
-    user = db.session.query(custprof).filter(custprof.username == username).first()
+    user = db.session.query(custdetails).filter(custdetails.username == username).first()
     db.session.commit()
     if request.method == 'POST':
         user.aadhar = request.form['aadhar']
@@ -231,6 +220,7 @@ def editprofile():
         user.prevmedrcrds = request.form['prevmedrcrds']
         user.address = request.form['address']
         user.pincode = request.form['pincode']
+        user.gmail_id = request.form['gmail_id']
         db.commit()
         flash('Profile Updated','success')
         return redirect(url_for('dashboard'))
@@ -265,7 +255,7 @@ def accident():
     username = session['username']
     pincode = session['pincode']
     list_of_hosp_to_send_message = []
-    profile = db.session.query(custprof).filter(custprof.username == username).first()
+    profile = db.session.query(custdetails).filter(custdetails.username == username).first()
     db.session.commit()
     if profile is not None:
         if db.session.query(orders).filter(orders.username == username).count() == 0:
@@ -278,14 +268,14 @@ def accident():
                 data = orders(hptl_username_in_vicinity, username, type, profile.address, profile.name, profile.aadhar, profile.age, profile.gender, profile.prevmedrcrds)
                 db.session.add(data)
                 db.session.commit()
-            return render_template('accident.html')
+            return render_template('request_sent.html')
         else:
             flash('you have already sent a request, kindly wait till it is processed','danger')
-            return render_template('dashboard.html')
+            return render_template('request_sent.html')
     else:
         flash('please fill in your details so that we can send it to the hospitals','danger')
         return redirect(url_for('add_profile'))
-    return render_template('accident.html')
+    return render_template('request_sent.html')
 
 @app.route('/heartattack')
 @is_logged_in
@@ -293,41 +283,28 @@ def heartattack():
     username = session['username']
     pincode = session['pincode']
     list_of_hosp_to_send_message = []
-    profile = db.session.query(custprof).filter(custprof.username == username).first()
+    profile = db.session.query(custdetails).filter(custdetails.username == username).first()
     db.session.commit()
     if profile is not None:
         if db.session.query(orders).filter(orders.username == username).count() == 0:
-            type='accident'
+            type='heart attack'
             for hospital in hospdetails:
                 if hospital.pincode == pincode_cust:
                     list_of_hosp_to_send_message.append(hospital.username)
             #send to all hospitals at the same time
-            for name_of_hptls_sent in list_of_hosp_to_send_message:
-                data = orders(name_of_hptls_sent, username, type, profile.address, profile.name, profile.aadhar, profile.age, profile.gender, profile.prevmedrcrds)
+            for hptl_username_in_vicinity in list_of_hosp_to_send_message:
+                data = orders(hptl_username_in_vicinity, username, type, profile.address, profile.name, profile.aadhar, profile.age, profile.gender, profile.prevmedrcrds)
                 db.session.add(data)
                 db.session.commit()
-            if db.session.query(result).filter(result.username_cust == username).count() == 1:
-                result = db.session.query(result).filter(result.username_cust == username).first()
-                row_to_be_sent_to_past_order = db.session.query(orders).filter_by(result.username_cust==username).first()
-                row = pastorders(row_to_be_sent_to_past_order)
-                db.session.add(row)
-                rows_to_be_deleted = db.session.query(orders).filter(orders.username_cust == username).all()
-                db.session.delete(rows_to_be_deleted)
-                hospdetails = db.session.query(hospdetails).all()
-                db.session.commit()
-                if result.acc_or_dec == "a":
-                    gilibiligilibi = result.name_of_hptl_result
-                    return redirect(url_for('saved'))
-                else:
-                    return redirect(url_for('not_saved'))
-            return render_template('heartattack.html')
+            return render_template('request_sent.html')
         else:
             flash('you have already sent a request, kindly wait till it is processed','danger')
-            return render_template('dashboard.html')
+            return render_template('request_sent.html')
     else:
         flash('please fill in your details so that we can send it to the hospitals','danger')
         return redirect(url_for('add_profile'))
-    return render_template('heartattack.html')
+    return render_template('request_sent.html')
+
 
 @app.route('/otherailments')
 @is_logged_in
@@ -335,41 +312,28 @@ def otherailments():
     username = session['username']
     pincode = session['pincode']
     list_of_hosp_to_send_message = []
-    profile = db.session.query(custprof).filter(custprof.username == username).first()
+    profile = db.session.query(custdetails).filter(custdetails.username == username).first()
     db.session.commit()
     if profile is not None:
         if db.session.query(orders).filter(orders.username == username).count() == 0:
-            type='accident'
+            type='other ailments'
             for hospital in hospdetails:
                 if hospital.pincode == pincode_cust:
                     list_of_hosp_to_send_message.append(hospital.username)
             #send to all hospitals at the same time
-            for name_of_hptls_sent in list_of_hosp_to_send_message:
-                data = orders(name_of_hptls_sent, username, type, profile.address, profile.name, profile.aadhar, profile.age, profile.gender, profile.prevmedrcrds)
+            for hptl_username_in_vicinity in list_of_hosp_to_send_message:
+                data = orders(hptl_username_in_vicinity, username, type, profile.address, profile.name, profile.aadhar, profile.age, profile.gender, profile.prevmedrcrds)
                 db.session.add(data)
                 db.session.commit()
-            if db.session.query(result).filter(result.username_cust == username).count() == 1:
-                result = db.session.query(result).filter(result.username_cust == username).first()
-                row_to_be_sent_to_past_order = db.session.query(orders).filter_by(result.username_cust=username).first()
-                row = pastorders(row_to_be_sent_to_past_order)
-                db.session.add(row)
-                rows_to_be_deleted = db.session.query(orders).filter(orders.username_cust == username).all()
-                db.session.delete(rows_to_be_deleted)
-                hospdetails = db.session.query(hospdetails).all()
-                db.session.commit()
-                if result.acc_or_dec == "a":
-                    gilibiligilibi = result.name_of_hptl_result
-                    return redirect(url_for('saved'))
-                else:
-                    return redirect(url_for('not_saved'))
-            return render_template('otherailments.html')
+            return render_template('request_sent.html')
         else:
             flash('you have already sent a request, kindly wait till it is processed','danger')
-            return render_template('dashboard.html')
+            return render_template('request_sent.html')
     else:
         flash('please fill in your details so that we can send it to the hospitals','danger')
         return redirect(url_for('add_profile'))
-    return render_template('otherailments.html')
+    return render_template('request_sent.html')
+
 
 class pastorders(db.Model):
     __tablename__ = 'pastorders'
@@ -420,28 +384,34 @@ class result(db.Model):
 @app.route('/accepted/<username>')
 @is_logged_in
 def accepted(username):
-        if db.session.query(result).filter(result.username_cust == username).count() == 0:
-            acc_or_dec = "a"
-            name_of_hptl_result = session['name']
-            username_cust = username
-            data = result(name_of_hptl_result, username, acc_or_dec)
-            db.session.add(data)
-            db.session.commit()
-            flash('you have accepted to save '+username, 'success')
-            return render_template('dashboardmnmg.html')
+    if db.session.query(orders).filter(orders.username_cust == username).count() == 1:
+        acc_or_dec = "a"
+        name_of_hptl_result = session['name']
+        username_cust = username
+
+        #send mail to that person!!!!! important
+        user = db.session.query(custdetails).filter(custdetails.username == username).first()
+        gmail_id = user.gmail_id
+        #delete data from orders
+        #add data to past orders
+        flash('you have accepted to save '+username, 'success')
+    return render_template('dashboardmnmg.html')
 
 @app.route('/declined/<username>')
 @is_logged_in
 def declined(username):
-        if db.session.query(result).filter(result.username_cust == username).count() == 0:
-            acc_or_dec = "d"
-            name_of_hptl_result = session['name']
-            username_cust = username
-            data = result(name_of_hptl_result, username, acc_or_dec)
-            db.session.add(data)
-            db.session.commit()
-            flash('you have declined to save '+username, 'danger')
-            return render_template('dashboardmnmg.html')
+    if db.session.query(orders).filter(orders.username_cust == username).count() == 1:
+        acc_or_dec = "d"
+        name_of_hptl_result = session['name']
+        username_cust = username
+
+        #send mail to that person!!!!! important
+        user = db.session.query(custdetails).filter(custdetails.username == username).first()
+        gmail_id = user.gmail_id
+        #delete data from orders
+        #add data to past orders
+        flash('you have declined to save '+username, 'danger')
+    return render_template('dashboardmnmg.html')
 
 if __name__=='__main__':
     app.secret_key='secret123'
