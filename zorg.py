30a905ddbbb49673056f9b84da62da2fd3a04ed3
Zorg66 <zorg123546@gmail.com>
2020-11-08 16:27:01 +0530

    changed database

--- zorg.py
+++ zorg.py
@@ -23,7 +23,7 @@ if ENV=='dev':
         app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:Tarun@postgresql@localhost/Zorg'
 else:
     app.debug=False
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://clrlgimqhrehyz:98975c8866eb814bdf030d4b06e12c62f1a643b09f35df320b476efc4e890e2f@ec2-100-25-100-81.compute-1.amazonaws.com:5432/d3ftva6r4n71f'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://hxceiafawftfoj:be308eb925667514c2f0102ea54672bd5b11a3c6643062b2012dedc396304b36@ec2-54-237-155-151.compute-1.amazonaws.com:5432/d82ngqr88afvm9'
     app.config['SECRET_KEY'] = os.environ['secret']
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
